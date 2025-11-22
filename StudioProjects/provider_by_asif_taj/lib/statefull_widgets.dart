d519bc5278db956250125292ad44f8c46220a9b7
Muhammad Ismail <ismailcoder366@gmail.com>
2025-11-22 05:44:46 +0500

    statefull widget added

--- StudioProjects/provider_by_asif_taj/lib/statefull_widgets.dart
+++ StudioProjects/provider_by_asif_taj/lib/statefull_widgets.dart
@@ -2,13 +2,39 @@ import 'package:flutter/material.dart';
 class StatefullWidgets extends StatefulWidget {
   const StatefullWidgets({super.key});
 
+
+
   @override
   State<StatefullWidgets> createState() => _StatefullWidgetsState();
 }
 
 class _StatefullWidgetsState extends State<StatefullWidgets> {
+
+  int x = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print('build');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('statefull widgets'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Center(child: Text(x.toString(), style: TextStyle(fontSize: 50),)),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        setState(() {
+          x++;
+        });
+      },
+      child: Icon(Icons.add),
+      ),
+    );
   }
 }
