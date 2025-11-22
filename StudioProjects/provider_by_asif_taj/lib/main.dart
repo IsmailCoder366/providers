a5090dfe254e816eba85eb5b350b6acf4572fcd6
Muhammad Ismail <ismailcoder366@gmail.com>
2025-11-23 02:23:55 +0500

    count exampl with provide added

--- StudioProjects/provider_by_asif_taj/lib/main.dart
+++ StudioProjects/provider_by_asif_taj/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_by_asif_taj/provider/count_provider.dart';
+import 'package:provider_by_asif_taj/screens/count_example.dart';
 import 'package:provider_by_asif_taj/statefull_widgets.dart';
 import 'package:provider_by_asif_taj/stateless_widgets.dart';
 import 'package:provider_by_asif_taj/why_provider.dart';
@@ -14,11 +17,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-
-      home:  WhyProvider(),
+    return ChangeNotifierProvider(create: (_)=>CountProvider(),
+    child: MaterialApp(
+      title: ('provider example'),
+      home: CountExample(),
+    ),
     );
+
   }
 }
 
--- StudioProjects/provider_by_asif_taj/lib/screens/count_example.dart
+++ StudioProjects/provider_by_asif_taj/lib/screens/count_example.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_by_asif_taj/provider/count_provider.dart';
 
 class CountExample extends StatefulWidget {
   const CountExample({super.key});
@@ -8,8 +12,34 @@ class CountExample extends StatefulWidget {
 }
 
 class _CountExampleState extends State<CountExample> {
+
+  void initState(){
+    final countProvider = context.read<CountProvider>();
+    Timer.periodic(Duration(seconds: 1), (timer){
+      countProvider.setCount();
+    });
+  }
+
   @override
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print('ismail');
+    final countprovider = context.read<CountProvider>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider Example'),
+      ),
+      body: Consumer<CountProvider>(builder: (context, value, child){
+        print('khan');
+        return Center(
+          child: Text(value.count.toString(), style: TextStyle(fontSize: 50),),
+        );
+      }),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        countprovider.setCount();
+      },
+      child: Icon(Icons.add),
+      ),
+    );
   }
 }
